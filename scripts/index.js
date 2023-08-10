df2d1418e938938bcb3da7ec932b86b12148648d
Tolu Richards <122994822+tolurichards@users.noreply.github.com>
2023-08-10 14:34:45 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Aug 9, 2023',
+  birthdate: 'Aug 11, 2023',
   name: 'Beautiful Bella'
 };
 
